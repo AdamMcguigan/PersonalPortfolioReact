aacb2943648c616e39b919bcd7503e3e4fa141da
Adam Mcguigan <c00248515@itcarlow.ie>
2023-06-22 18:03:38 +0100

    Navbar and Header
    Navbar is finished, started the header

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './Components/NavBar';
+import { Banner } from './Components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">x
+      <NavBar />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
